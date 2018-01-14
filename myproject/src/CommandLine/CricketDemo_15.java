bc2b6339e3aeab3e85eebf5bff731c14092e2bc6
Rushik Maniar <rushikmaniar107@gmail.com>
2018-01-14 23:06:37 +0530

    small change in stringdemo5 and CricketDemo created

--- myproject/src/CommandLine/CricketDemo_15.java
+++ myproject/src/CommandLine/CricketDemo_15.java
@@ -4,19 +4,24 @@ import java.util.*;
 
 class CricketDemo_15 {
 	public static void main(String args[]) {
-		ArrayList<String> names = new ArrayList<String>();
-		ArrayList<Double> marks = new ArrayList<Double>();
-		ArrayList<Double> ind_Total = new ArrayList<Double>();
-		for (int i = 0; i < 4; i++) {
-			double local_total = 0 ;
-			if (i == 1) {
-				names.add(args[i]);
-			} else {
-				double mark = Double.parseDouble(args[i]);
-				local_total = local_total + marks;
-				marks.add(mark);
+		
+	String[] names = new String[5];
+	 int count = 0;
+	double[][] run = new double[4][4];
+	for(int i=0 ;i < names.length-1;i++) {	
+		for (int j = 0; j < names.length-1; j++) {
+			
+			if (j == 0) {
+				names[j] = args[count++];
 			}
+			else {
+				run[i][j] = Double.parseDouble(args[count++]);
+			}
+			
 		}
-
 	}
+	for(String name : names) {
+		System.out.print(" " + name);
+	}
+}
 }
